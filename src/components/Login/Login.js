2eba8a81cad4a30f5b8dadceb9b350f88e35645a
Daniela Danailova <d.k.danailova@gmail.com>
2019-02-28 14:02:08 +0200

    Added Form binding

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,86 +1,121 @@
 import React, {Component} from 'react';
-import { toast } from 'react-toastify';
+import BindingForm from '../BindingForms/BindingForms';
 
-class Login extends Component{
-    constructor(props){
-        super(props);
+class Login extends Component {
 
-        this.state={
-            username:'',
-            password:''
-        }
-    }
-
-    handelSubmit = (ev)=>{
+    handelSubmit = (ev, data)=>{
         ev.preventDefault();
-        this.props.login(this.state);
-    }
-
-    handleInputChange =(ev)=>{
-        const {name, value}=ev.target;
-        this.setState({
-            [name]:value
-        });
-    }
-
-    checkInputValidation=(ev)=>{
-        const {target}=ev;
-        if(!target.checkValidity()){
-          toast.error(target.validationMessage, {
-            closeButton: false
-          });
-        }
+        let userData = {...data};
+        delete userData.errors;
+        this.props.login(userData);
     }
 
     render(){
-        const {username, password}=this.state;
-        return(
+        return (
             <main className='site-content guest'>
                 <section className="site-login">
-                    <form onSubmit={this.handelSubmit}>
-                        <h2>Login</h2>
-                        <p className="field">
-                            <label htmlFor="username">Username</label>
-                            <span className="input">
-                                <input 
-                                    type="text" 
-                                    id="username" 
-                                    name="username"
-                                    value={username}
-                                    onChange={this.handleInputChange}
-                                    onBlur={this.checkInputValidation}
-                                    minLength="3"
-                                    maxLength="15"
-                                />
-                                <span className="actions"></span>
-                                <i className="fas fa-user"></i>
-                            </span>
-                            {/* <span class="form-error">The field is required</span> */}
-                        </p>
-                        <p className="field">
-                            <label htmlFor="password">Password</label>
-                            <span className="input">
-                                <input 
-                                    type="password" 
-                                    id="password" 
-                                    name="password"
-                                    value={password}
-                                    onChange={this.handleInputChange}
-                                    onBlur={this.checkInputValidation}
-                                    minLength="5"
-                                    maxLength="15"
-                                />
-                                <span className="actions"></span>
-                                <i className="fas fa-key"></i>
-                            </span>
-                            {/* <span class="form-error">The field is required</span> */}
-                        </p>
-                        <input type="submit" className="button" value="Login"/>
-                    </form>
+                    <BindingForm formType='Login' onSubmit={this.handelSubmit}>
+                       <input 
+                            type="text" 
+                            id="username" 
+                            name="username"
+                            minLength="3"
+                            maxLength="15"
+                        />
+                        <input 
+                            type="password" 
+                            id="password" 
+                            name="password"
+                            minLength="5"
+                            maxLength="15"
+                        />
+                    </BindingForm>
                 </section>
             </main>
         )
     }
 }
+
+// class Login extends Component{
+//     constructor(props){
+//         super(props);
+
+//         this.state={
+//             username:'',
+//             password:''
+//         }
+//     }
+
+//     handelSubmit = (ev)=>{
+//         ev.preventDefault();
+//         this.props.login(this.state);
+//     }
+
+//     handleInputChange =(ev)=>{
+//         const {name, value}=ev.target;
+//         this.setState({
+//             [name]:value
+//         });
+//     }
+
+//     checkInputValidation=(ev)=>{
+//         const {target}=ev;
+//         if(!target.checkValidity()){
+//           toast.error(target.validationMessage, {
+//             closeButton: false
+//           });
+//         }
+//     }
+
+//     render(){
+//         const {username, password}=this.state;
+//         return(
+//             <main className='site-content guest'>
+//                 <section className="site-login">
+//                     <form onSubmit={this.handelSubmit}>
+//                         <h2>Login</h2>
+//                         <p className="field">
+//                             <label htmlFor="username">Username</label>
+//                             <span className="input">
+//                                 <input 
+//                                     type="text" 
+//                                     id="username" 
+//                                     name="username"
+//                                     value={username}
+//                                     onChange={this.handleInputChange}
+//                                     onBlur={this.checkInputValidation}
+//                                     minLength="3"
+//                                     maxLength="15"
+//                                 />
+//                                 <span className="actions"></span>
+//                                 <i className="fas fa-user"></i>
+//                             </span>
+//                             {/* <span class="form-error">The field is required</span> */}
+//                         </p>
+//                         <p className="field">
+//                             <label htmlFor="password">Password</label>
+//                             <span className="input">
+//                                 <input 
+//                                     type="password" 
+//                                     id="password" 
+//                                     name="password"
+//                                     value={password}
+//                                     onChange={this.handleInputChange}
+//                                     onBlur={this.checkInputValidation}
+//                                     minLength="5"
+//                                     maxLength="15"
+//                                 />
+//                                 <span className="actions"></span>
+//                                 <i className="fas fa-key"></i>
+//                             </span>
+//                             {/* <span class="form-error">The field is required</span> */}
+//                         </p>
+//                         <input type="submit" className="button" value="Login"/>
+//                     </form>
+//                 </section>
+//             </main>
+//         )
+//     }
+// }
 
 export default Login;
--- src/components/Register/Register.js
+++ src/components/Register/Register.js
@@ -1,175 +1,242 @@
 import React, {Component} from 'react';
+import BindingForm from './../BindingForms/BindingForms';
 import { toast } from 'react-toastify';
 
+class Register extends Component {
 
-class Register extends Component{
-    constructor(props){
-        super(props);
-
-        this.state={
-            username:'',
-            password:'',
-            repeatPassword:'',
-            fullName:'',
-            email:'',
-            phone:''
-        }
-    }
-
-    handelSubmit = (ev)=>{
+    handelSubmit = (ev, data)=>{
         ev.preventDefault();
-        let userData = {...this.state};
-        delete userData.repeatPassword;
-        this.props.register(userData);
-    }
-
-    handleInputChange =(ev)=>{
-        const {name, value}=ev.target;
-        this.setState({
-            [name]:value
-        });
-    }
-
-    checkInputValidation=(ev)=>{
-        const {target}=ev;
-        if(!target.checkValidity()){
-          toast.error(target.validationMessage, {
-            closeButton: false
-          });
+        let userData = {...data};
+        if(userData.password !== userData['repeat-password']){
+            toast.error('Both passwords should match!' , {
+                closeButton: false
+              });
+        }else{
+            delete userData['repeat-password'];
+            delete userData.errors;
+            this.props.register(userData);
         }
-        // if(this.state.password !== this.state.repeatPassword){
-        //     toast.error('The passwords must match', {
-        //         closeButton: false
-        //       });
-        // }
     }
-
 
     render(){
-        const {
-            username,
-            password,
-            repeatPassword,
-            fullName,
-            email,
-            phone
-        }=this.state;
-
-        return(
+        return (
             <main className='site-content guest'>
-                <section className="site-signup">
-                    <form onSubmit={this.handelSubmit}>
-                        <h2>Register</h2>
-                        <p className="field">
-                            <label htmlFor="username">Username</label>
-                            <span className="input">
-                                <input 
-                                    type="text" 
-                                    id="username" 
-                                    name="username"
-                                    value={username}
-                                    onChange={this.handleInputChange}
-                                    onBlur={this.checkInputValidation}
-                                    minLength="3"
-                                    maxLength="15"
-                                />
-                                <span className="actions"></span>
-                                <i className="fas fa-user"></i>
-                            </span>
-                            {/* <span class="form-error">The field is required</span> */}
-                        </p>
-                        <p className="field">
-                            <label htmlFor="password">Password</label>
-                            <span className="input">
-                                <input 
-                                    type="password" 
-                                    id="password" 
-                                    name="password"
-                                    value={password}
-                                    onChange={this.handleInputChange}
-                                    onBlur={this.checkInputValidation}
-                                    minLength="5"
-                                    maxLength="15"
-                                />
-                                <span className="actions"></span>
-                                <i className="fas fa-key"></i>
-                            </span>
-                            {/* <span class="form-error">The field is required</span> */}
-                        </p>
-                        <p className="field">
-                            <label htmlFor="confirm-password">Repeat Password</label>
-                            <span className="input">
-                                <input 
-                                    type="password" 
-                                    id="repeat-password" 
-                                    name="repeatPassword"
-                                    value={repeatPassword}
-                                    onChange={this.handleInputChange}
-                                    onBlur={this.checkInputValidation}
-                                    pattern={this.state.password}
-                                />
-                                <span className="actions"></span>
-                                <i className="fas fa-key"></i>
-                            </span>
-                            {/* <span class="form-error">The field is required</span> */}
-                        </p>
-                        <p className="field">
-                            <label htmlFor="full-name">Full Name</label>
-                            <span className="input">
-                                <input 
-                                    type="text" 
-                                    id="full-name" 
-                                    name="fullName"
-                                    value={fullName}
-                                    onChange={this.handleInputChange}
-                                    onBlur={this.checkInputValidation}
-                                    minLength="3"
-                                    maxLength="25"
-                                />
-                                <span className="actions"></span>
-                                <i className="fas fa-user"></i>
-                            </span>
-                            {/* <span class="form-error">The field is required</span> */}
-                        </p>
-                        <p className="field">
-                            <label htmlFor="email">E-mail</label>
-                            <span className="input">
-                                <input 
-                                    type="email" 
-                                    id="email" 
-                                    name="email"
-                                    value={email}
-                                    onChange={this.handleInputChange}
-                                    onBlur={this.checkInputValidation}
-                                    pattern="[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$"
-                                />
-                                <span className="actions"></span>
-                                <i className="fas fa-envelope"></i>
-                            </span>
-                            {/* <span class="form-error">The field is required</span> */}
-                        </p>
-                        <p className="field">
-                            <label htmlFor="phone">Phone</label>
-                            <span className="input">
-                                <input 
-                                    type="tel" 
-                                    id="phone" 
-                                    name="phone"
-                                    value={phone}
-                                    onChange={this.handleInputChange}
-                                    onBlur={this.checkInputValidation}
-                                />
-                                <span className="actions"></span>
-                                <i className="fas fa-phone"></i>
-                            </span>
-                            {/* <span class="form-error">The field is required</span> */}
-                        </p>
-                        <input type="submit" className="button" value="Signup"/>
-                    </form>
+                <section className="site-login">
+                    <BindingForm formType='Register' onSubmit={this.handelSubmit}>
+                       <input 
+                            type="text" 
+                            id="username" 
+                            name="username"
+                            minLength="3"
+                            maxLength="15"
+                        />
+                        <input 
+                            type="password" 
+                            id="password" 
+                            name="password"
+                            minLength="5"
+                            maxLength="15"
+                        />
+                        <input
+                            type="password" 
+                            id="repeat-password" 
+                            name="repeat-password"
+                            minLength="5"
+                            maxLength="15"
+                        />
+                        <input
+                            type="text" 
+                            id="full-name" 
+                            name="full-name"
+                            minLength="3"
+                            maxLength="25"
+                        />
+                        <input
+                            type="email" 
+                            id="email" 
+                            name="email"
+                            pattern="[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$"
+                        />
+                        <input
+                            type="tel" 
+                            id="phone" 
+                            name="phone"
+                        />
+                    </BindingForm>
                 </section>
             </main>
         )
     }
 }
+
+// class Register extends Component{
+//     constructor(props){
+//         super(props);
+
+//         this.state={
+//             username:'',
+//             password:'',
+//             repeatPassword:'',
+//             fullName:'',
+//             email:'',
+//             phone:''
+//         }
+//     }
+
+//     handelSubmit = (ev)=>{
+//         ev.preventDefault();
+//         let userData = {...this.state};
+//         delete userData.repeatPassword;
+//         this.props.register(userData);
+//     }
+
+//     handleInputChange =(ev)=>{
+//         const {name, value}=ev.target;
+//         this.setState({
+//             [name]:value
+//         });
+//     }
+
+//     checkInputValidation=(ev)=>{
+//         const {target}=ev;
+//         if(!target.checkValidity()){
+//           toast.error(target.validationMessage, {
+//             closeButton: false
+//           });
+//         }
+//         // if(this.state.password !== this.state.repeatPassword){
+//         //     toast.error('The passwords must match', {
+//         //         closeButton: false
+//         //       });
+//         // }
+//     }
+
+
+//     render(){
+//         const {
+//             username,
+//             password,
+//             repeatPassword,
+//             fullName,
+//             email,
+//             phone
+//         }=this.state;
+
+//         return(
+//             <main className='site-content guest'>
+//                 <section className="site-signup">
+//                     <form onSubmit={this.handelSubmit}>
+//                         <h2>Register</h2>
+//                         <p className="field">
+//                             <label htmlFor="username">Username</label>
+//                             <span className="input">
+//                                 <input 
+//                                     type="text" 
+//                                     id="username" 
+//                                     name="username"
+//                                     value={username}
+//                                     onChange={this.handleInputChange}
+//                                     onBlur={this.checkInputValidation}
+//                                     minLength="3"
+//                                     maxLength="15"
+//                                 />
+//                                 <span className="actions"></span>
+//                                 <i className="fas fa-user"></i>
+//                             </span>
+//                             {/* <span class="form-error">The field is required</span> */}
+//                         </p>
+//                         <p className="field">
+//                             <label htmlFor="password">Password</label>
+//                             <span className="input">
+//                                 <input 
+//                                     type="password" 
+//                                     id="password" 
+//                                     name="password"
+//                                     value={password}
+//                                     onChange={this.handleInputChange}
+//                                     onBlur={this.checkInputValidation}
+//                                     minLength="5"
+//                                     maxLength="15"
+//                                 />
+//                                 <span className="actions"></span>
+//                                 <i className="fas fa-key"></i>
+//                             </span>
+//                             {/* <span class="form-error">The field is required</span> */}
+//                         </p>
+//                         <p className="field">
+//                             <label htmlFor="confirm-password">Repeat Password</label>
+//                             <span className="input">
+//                                 <input 
+//                                     type="password" 
+//                                     id="repeat-password" 
+//                                     name="repeatPassword"
+//                                     value={repeatPassword}
+//                                     onChange={this.handleInputChange}
+//                                     onBlur={this.checkInputValidation}
+//                                     pattern={this.state.password}
+//                                 />
+//                                 <span className="actions"></span>
+//                                 <i className="fas fa-key"></i>
+//                             </span>
+//                             {/* <span class="form-error">The field is required</span> */}
+//                         </p>
+//                         <p className="field">
+//                             <label htmlFor="full-name">Full Name</label>
+//                             <span className="input">
+//                                 <input 
+//                                     type="text" 
+//                                     id="full-name" 
+//                                     name="fullName"
+//                                     value={fullName}
+//                                     onChange={this.handleInputChange}
+//                                     onBlur={this.checkInputValidation}
+//                                     minLength="3"
+//                                     maxLength="25"
+//                                 />
+//                                 <span className="actions"></span>
+//                                 <i className="fas fa-user"></i>
+//                             </span>
+//                             {/* <span class="form-error">The field is required</span> */}
+//                         </p>
+//                         <p className="field">
+//                             <label htmlFor="email">E-mail</label>
+//                             <span className="input">
+//                                 <input 
+//                                     type="email" 
+//                                     id="email" 
+//                                     name="email"
+//                                     value={email}
+//                                     onChange={this.handleInputChange}
+//                                     onBlur={this.checkInputValidation}
+//                                     pattern="[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$"
+//                                 />
+//                                 <span className="actions"></span>
+//                                 <i className="fas fa-envelope"></i>
+//                             </span>
+//                             {/* <span class="form-error">The field is required</span> */}
+//                         </p>
+//                         <p className="field">
+//                             <label htmlFor="phone">Phone</label>
+//                             <span className="input">
+//                                 <input 
+//                                     type="tel" 
+//                                     id="phone" 
+//                                     name="phone"
+//                                     value={phone}
+//                                     onChange={this.handleInputChange}
+//                                     onBlur={this.checkInputValidation}
+//                                 />
+//                                 <span className="actions"></span>
+//                                 <i className="fas fa-phone"></i>
+//                             </span>
+//                             {/* <span class="form-error">The field is required</span> */}
+//                         </p>
+//                         <input type="submit" className="button" value="Signup"/>
+//                     </form>
+//                 </section>
+//             </main>
+//         )
+//     }
+// }
 
 export default Register;
